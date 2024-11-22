5169ccf043f9b7c2eabe86a713b47f75897554db
relly27 <rellyalbertogarciapacheco_@hotmail.com>
2024-11-22 10:23:51 -0400

    update

--- src/controller/main.js
+++ src/controller/main.js
@@ -24,5 +24,5 @@ async function includeHTML(selector, url) {
   
   // Incluir las vistas
   includeHTML('#footerID', '/../src/view/footer.html'); 
-  includeHTML('#nav-barP', '/../src/view/nav-bar.html'); 
+  includeHTML('#navbarID', '/../src/view/nav-bar.html'); 
   
